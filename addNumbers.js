50a63fd77afd674f55335d58bcda4f36835646dc
abaez8031 <abaez8031@gmail.com>
2023-03-02 16:22:44 -0500

    finish addNumbers

---
+++ addNumbers.js
@@ -0,0 +1,20 @@
+const readline = require("readline");
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function addNumbers(sum, numsLeft, completionCallback) {
+  if (numsLeft === 0) {
+    completionCallback(sum);
+  } else {
+    reader.question("Please input a number", function (answer) {
+      sum += parseInt(answer);
+      console.log(sum);
+      addNumbers(sum, numsLeft - 1, completionCallback);
+    });
+  }
+}
+
+addNumbers(0, 3, (sum) => console.log(`Total Sum: ${sum}`));
